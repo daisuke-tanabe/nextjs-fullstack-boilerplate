83d792a78cf9039e18f7c3ba5db6d20df7a6d325
daisuke-tanabe <dev.daisuke.tanabe@gmail.com>
2024-10-31 07:18:37 +0900

    feat: add navigation from email to confirm page

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -28,14 +28,10 @@ export async function login(formData: FormData) {
 export async function signup(formData: FormData) {
   const supabase = await createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
+  const { error } = await supabase.auth.signUp({
     email: formData.get('email') as string,
     password: formData.get('password') as string,
-  };
-
-  const { error } = await supabase.auth.signUp(data);
+  });
 
   if (error) {
     redirect('/error');
