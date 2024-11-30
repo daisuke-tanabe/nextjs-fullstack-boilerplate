c92a0c44811584ce412244171c9bd061b08037f6
daisuke-tanabe <dev.daisuke.tanabe@gmail.com>
2024-11-30 19:40:54 +0900

    chore: improve UI layout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,22 @@
+import { Button, Input } from '@nextui-org/react';
+
 import { login, signup } from './actions';
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="flex w-full max-w-sm flex-col gap-4 rounded-large px-8 pb-10 pt-6">
+        <form className="flex flex-col gap-4">
+          <Input label="Email" id="email" name="email" type="email" required />
+          <Input label="password" id="password" name="password" type="password" required />
+          <Button color="primary" type="submit" formAction={login}>
+            Log in
+          </Button>
+          <Button color="primary" type="submit" formAction={signup}>
+            Sign up
+          </Button>
+        </form>
+      </div>
+    </div>
   );
 }
