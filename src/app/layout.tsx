755d3a831d18d4029a11818563378776191cc05b
daisuke-tanabe <dev.daisuke.tanabe@gmail.com>
2024-11-30 20:52:21 +0900

    feat: add navigation header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { NextUIProvider } from '@nextui-org/react';
+import { Button, Navbar, NavbarBrand, NavbarContent, NextUIProvider } from '@nextui-org/react';
 import './globals.css';
+import Image from 'next/image';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +19,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NextUIProvider>{children}</NextUIProvider>
+        <NextUIProvider>
+          <Navbar maxWidth="full" isBordered>
+            <NavbarBrand>
+              <Image src="/vercel.svg" alt="Next.js Logo" width={97} height={22} priority />
+            </NavbarBrand>
+            <NavbarContent justify="end">
+              <Button className="text-default-500" radius="full" variant="light">
+                Login
+              </Button>
+              <Button className="text-default-500" radius="full" variant="light">
+                Signup
+              </Button>
+            </NavbarContent>
+          </Navbar>
+          <main className="p-6">{children}</main>
+        </NextUIProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { prisma } from '@/lib';
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
@@ -15,14 +14,9 @@ export default async function Home() {
   const result = await prisma.user.count();
 
   return (
-    <main className="flex w-full max-w-sm flex-col gap-4 rounded-large px-8 pb-10 pt-6">
-      <div>
-        <Image src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
-      </div>
-      <div>
-        <div>Your email: {user.email}</div>
-        <div>Total users: {result}</div>
-      </div>
-    </main>
+    <div>
+      <div>Your email: {user.email}</div>
+      <div>Total users: {result}</div>
+    </div>
   );
 }
